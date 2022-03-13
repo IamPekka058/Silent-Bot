342ef4f5b7894ed97440d6816c1f811664fa728d
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2022-03-13 02:15:27 +0100

    Update

--- Installer/install.py
+++ Installer/install.py
@@ -6,9 +6,16 @@ def collectPackages():
     print("Collect packages...")
     if(platform.system == "Linux"):
         p = subprocess.Popen("sudo pip install -r requirements.txt", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        p.wait()
+        if(p.returncode != 1):
+            print("Pip is not installed.\nInstalling...")
+            p = subprocess.Popen("sudo apt-get install python3-pip", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            p.wait()
+            print("Pip installed\n")
+            collectPackages()
     else:
         p = subprocess.Popen("pip install -r requirements.txt", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    p.wait()
+        p.wait()
     print("Collected all packages.")
 
 if __name__ == "__main__":
